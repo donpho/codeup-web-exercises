eba199030693f471e79e1c8a729c565e0b61e389
donpho <39744333+donpho@users.noreply.github.com>
2018-06-20 11:47:29 -0500

    trying to refactor the code into a function

--- js/planets-string.js
+++ js/planets-string.js
@@ -27,49 +27,80 @@
 
     // planetsArray.unshift('PlaceHolder');
     // var planets = planetsArray;
-    // Add a placeholder in front of the array
+    // console.log(planets);
+    // ////Add a placeholder in front of the array
     // planets = planets.join('*<br> ');
-    // Made the array into a string with '*<br> '
+    // console.log(planets);
+    // ////Made the array into a string with '*<br> '
     // planets = planets.split('*'); // Indicating where the split is going to be and the rest of the tag will go right if split left and vice versa.
+    // console.log(planets);
     // planets.shift(); // Removed the PlaceHolder as it served its purpose of allowing us to join array with <br> to the actual beginning of the array.
+    // console.log(planets);
 
     // The code below will add '<li>' to the front of each array element.
 
-    planetsArray.unshift('PlaceHolder');
-    var planets = planetsArray;
-    planets = planets.join('*\n<li> ');
-    planets = planets.split('*');
-    planets.shift();
+    function addTags(arr, openingTag, closingTag) {
+        var fill = 'PlaceHolder';
+        var splitHere = '*';
+        var spacing = ' ';
 
-    // The code below will add '</li>' to the end of each array element.
+        var addSymbol = arr.slice();
+        addSymbol.unshift(fill);
+        addSymbol.push(fill);
 
-    planets.push('PlaceHolder');
-    planets = planets.join(' </li>8');
-    planets = planets.split('8');
-    planets.pop();
+        addSymbol = addSymbol.join(splitHere + '\n'+ openingTag + spacing);
+        addSymbol = addSymbol.split(splitHere);
+        addSymbol.shift();
 
-    // The code bellow will take out the first element of the array, then add '<ul>' in front of the element that was pulled out and then added it back to the original array
+        addSymbol = addSymbol.join(spacing + closingTag + splitHere );
+        addSymbol = addSymbol.split(splitHere);
+        addSymbol.pop();
 
-    var list = planets.shift();
-    list = list.split();
-    list.unshift('PlaceHolder');
-    list = list.join('^<ul> ');
-    list = list.split('^');
-    list.shift();
+        console.log(addSymbol.join(" "));
 
-    planets.unshift(list.shift());
+    }
+    addTags(planetsArray,'<br>','');
+    addTags(planetsArray,'<li>','</li>');
+    console.log(planetsArray);
 
-    // Same thing as above but instead of the first element of the array, it will apply to the last element of the array and added the '</ul>' at the end of the element
-
-    var end = planets.pop();
-    end = end.split();
-    end.push('PlaceHolder');
-    end = end.join(' \n</ul>^');
-    end = end.split('^');
-    end.pop();
-
-    planets.push(end.shift());
-    planets = planets.join(' ');
-    console.log(planets);
+    // planetsArray.unshift('PlaceHolder');
+    // var planets = planetsArray;
+    // planets = planets.join('*\n<li> ');
+    // console.log(planets);
+    // planets = planets.split('*');
+    // console.log(planets);
+    // planets.shift();
+    // console.log(planets);
+    //
+    // // The code below will add '</li>' to the end of each array element.
+    //
+    // planets.push('PlaceHolder');
+    // planets = planets.join(' </li>8');
+    // planets = planets.split('8');
+    // planets.pop();
+    //
+    // // The code bellow will take out the first element of the array, then add '<ul>' in front of the element that was pulled out and then added it back to the original array
+    //
+    // var list = planets.shift();
+    // list = list.split();
+    // list.unshift('PlaceHolder');
+    // list = list.join('^<ul> ');
+    // list = list.split('^');
+    // list.shift();
+    //
+    // planets.unshift(list.shift());
+    //
+    // // Same thing as above but instead of the first element of the array, it will apply to the last element of the array and added the '</ul>' at the end of the element
+    // var fill = 'PlaceHolder';
+    // var end = planets.pop();
+    // end = end.split();
+    // end.push(fill);
+    // end = end.join(' \n</ul>^');
+    // end = end.split('^');
+    // end.pop();
+    //
+    // planets.push(end.shift());
+    // planets = planets.join(' ');
+    // console.log(planets);
 
 })();
