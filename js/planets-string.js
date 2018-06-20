49b1b5a74dada79aad43c3d9ad5a3125aa0e8575
donpho <39744333+donpho@users.noreply.github.com>
2018-06-20 09:28:44 -0500

    cleaning up all the console logs

--- js/planets-string.js
+++ js/planets-string.js
@@ -27,66 +27,47 @@
 
     // planetsArray.unshift('PlaceHolder');
     // var planets = planetsArray;
-    // console.log(planets); // Add a placeholder in front of the array
+    // Add a placeholder in front of the array
     // planets = planets.join('*<br> ');
-    // console.log(planets); // Made the array into a string with '*<br> '
+    // Made the array into a string with '*<br> '
     // planets = planets.split('*'); // Indicating where the split is going to be and the rest of the tag will go right if split left and vice versa.
-    // console.log(planets);
     // planets.shift(); // Removed the PlaceHolder as it served its purpose of allowing us to join array with <br> to the actual beginning of the array.
-    // console.log(planets);
 
+    // The code below will add '<li>' to the front of each array element.
 
     planetsArray.unshift('PlaceHolder');
     var planets = planetsArray;
-    console.log(planets);
     planets = planets.join('*<li> ');
-    console.log(planets);
     planets = planets.split('*');
-    console.log(planets);
     planets.shift();
-    console.log(planets);
 
-    console.log('added "<li>" to the front');
-
-    // DO NOT TOUCH THE CODE ABOVE!!!!
+    // The code below will add '</li>' to the end of each array element.
 
     planets.push('PlaceHolder');
-    console.log(planets);
     planets = planets.join(' </li>8');
-    console.log(planets);
     planets = planets.split('8');
-    console.log(planets);
     planets.pop();
-    console.log(planets);
+
+    // The code bellow will take out the first element of the array, then add '<ul>' in front of the element that was pulled out and then added it back to the original array
 
     var list = planets.shift();
-    console.log(list);
     list = list.split();
-    console.log(list);
     list.unshift('PlaceHolder');
-    console.log(list);
     list = list.join('^<ul> ');
-    console.log(list);
     list = list.split('^');
-    console.log(list);
     list.shift();
-    console.log(list);
 
     planets.unshift(list.shift());
-    console.log(planets);
+
+    // Same thing as above but instead of the first element of the array, it will apply to the last element of the array and added the '</ul>' at the end of the element
 
     var end = planets.pop();
-    console.log(end);
-
     end = end.split();
     end.push('PlaceHolder');
     end = end.join(' </ul>^');
     end = end.split('^');
-    console.log(end);
     end.pop();
-    console.log(end);
 
-    console.log(planets);
     planets.push(end.shift());
     planets = planets.join(' ');
     console.log(planets);
